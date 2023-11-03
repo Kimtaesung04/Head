6af9e55fdba5d2db80c60a2b9f7fff3135a8aadc
Kim tae sung <xofhtm9804@gmail.com>
2023-11-03 20:33:47 +0900

    프로젝트 링크 연결(초기)

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Container } from "../components/Styles/Container/Container.styled" ;
+import { ProjectsConta } from '../components/Styles/section/Projects.styled';
 
 const Projects = () => {
     return (
         <Container>
-                <p>갸아아악</p>
-                <div></div>
+            <ProjectsConta>
+                <a href="https://kimtaesung-drum-sequencer.netlify.app/">
+                    <img src="./img/DrumSequencer.png" alt="" />
+                </a>
+                <a href="https://kimtaesung-todolist.netlify.app/">
+                    <img src="./img/ToDoList.png" alt="" />
+                </a>
+                </ProjectsConta>
         </Container>
     )
 }
